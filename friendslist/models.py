92231ea31995dce752a3f34cbd1f9db139e59567
kyantaro-man <67139948+kyantaro-man@users.noreply.github.com>
2021-06-13 13:18:50 +0900

    ユーザー作成直後にデフォルトカテゴリを作成する

--- friendslist/models.py
+++ friendslist/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class UserManager(BaseUserManager):
 
@@ -83,3 +85,10 @@ class Memo(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     friend = models.ForeignKey(Friend, on_delete=models.CASCADE)
+
+@receiver(post_save, sender=User)
+def create_category(sender, **kwargs):
+    if kwargs['created']:
+        default_category = Category.objects.create(user=kwargs['instance'])
+        default_category.name = "デフォルト"
+        default_category.save()
